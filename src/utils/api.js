2cea9b9985a4b80b5eb819be48199dbaecfea002
LukeHendrick <lukehendrick41@gmail.com>
2017-06-06 08:42:19 -0500

    changed api to https

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ var axios = require('axios');
 
 module.exports = {
   weather: function(location) {
-    var encodedURI = window.encodeURI('http://api.openweathermap.org/data/2.5/forecast/daily?q='
+    var encodedURI = window.encodeURI('https://api.openweathermap.org/data/2.5/forecast/daily?q='
     + location + '&units=imperial&cnt=5&appid=c4cd4d33febf26a19524fee9df8074e5')
 
     return axios.get(encodedURI).then(function(response) {
